1d482681dab93fa880abc9ff54bb0eaa20b48a03
Jianwei Gong <Jinle.Gong2014@gmail.com>
2022-04-08 16:56:46 -0700

    initial commit

---
+++ MyLinkedListCustomTester.java
@@ -0,0 +1,96 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+
+public class MyLinkedListCustomTester {
+
+    /**
+     * This sets up the test fixture. JUnit invokes this method before
+     * every testXXX method. The @Before tag tells JUnit to run this method
+     * before each test.
+     */
+    @Before
+    public void setUp() {
+
+    }
+
+    /**
+     * Test the add method when [TODO: fill in a possible edge case here]
+     */
+    @Test
+    public void testAdd() {
+
+    }
+
+    /**
+     * Test the add with index method when [TODO]
+     */
+    @Test
+    public void testAddWithIndexTestOne() {
+
+    }
+
+    /**
+     * Test the add with index method when [TODO: fill in another case here]
+     */	
+    @Test
+    public void testAddWithIndexTestTwo() {
+
+    }
+
+    /**
+     * Test the get method when [TODO]
+     */
+    @Test
+    public void testGet() {
+
+    }
+
+    /**
+     * Test the getNth method when [TODO]
+     */
+    @Test
+    public void testGetNth() {
+
+    }
+
+    /**
+     * Test the set method when [TODO]
+     */
+    @Test
+    public void testSet() {
+
+    }
+
+    /**
+     * Test the remove method when [TODO]
+     */
+    @Test
+    public void testRemoveTestOne() {
+
+    }
+    
+    /**
+     * Test the remove method when [TODO: fill in another one here]
+     */
+    @Test
+    public void testRemoveTestTwo() {
+
+    }
+
+    /**
+     * Test the clear method when [TODO]
+     */
+    @Test
+    public void testClear() {
+
+    }
+
+    /**
+     * Test the size method when [TODO]
+     */
+    @Test
+    public void testSize() {
+
+    }
+}
